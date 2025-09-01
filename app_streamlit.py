a6fb6ff6c4492615dfecee4f3c85f21e303bd4e6
Dudung VL <pianpurbalingga@gmail.com>
2025-09-01 10:08:12 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -4,13 +4,16 @@ import joblib
 
 model_tersimpan = joblib.load("model_mlbb.joblib")
 
-st.title("MLBB Clasification")
-kill = st.slider("Jumlah Kill",0,20)
-assist = st.slider("Jumlah Assist",0,20)
-death = st.slider("Jumlah Death",0,20)
-turret = st.slider("Jumlah Turret",0,20)
+st.title("MLBB Classification")
 
-if st.button("predict"):
-	data_button = pd.DataFrame([[kill,assist,death,turret]],columns=["kill","assist","death","turret"])
-	st.success(f"Hasil Prediksi : {model.predict([data_baru])[0]}")
-	st.ballons()
+kill = st.slider("Jumlah Kill", 0, 20)
+assist = st.slider("Jumlah Assist", 0, 20)
+death = st.slider("Jumlah Death", 0, 20)
+turret = st.slider("Jumlah Turret", 0, 20)
+if st.button("Predict"):
+    data_input = pd.DataFrame([[kill, assist, death, turret]],
+                              columns=["kill", "assist", "death", "turret"])
+    
+    hasil = model_tersimpan.predict(data_input)[0]
+    st.success(f"Hasil Prediksi : {hasil}")
+    st.balloons()
